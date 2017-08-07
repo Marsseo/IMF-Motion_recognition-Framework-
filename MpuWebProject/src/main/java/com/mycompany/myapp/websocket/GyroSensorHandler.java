eabedc135d33afc525be2653eb27cf1ad54984bc
SmileJM <lovelyeu@hanmail.net>
2017-08-07 10:57:14 +0900

    DB 계정 수정

--- MpuWebProject/src/main/java/com/mycompany/myapp/websocket/GyroSensorHandler.java
+++ MpuWebProject/src/main/java/com/mycompany/myapp/websocket/GyroSensorHandler.java
@@ -36,7 +36,7 @@ public class GyroSensorHandler extends TextWebSocketHandler implements Applicati
 	@PostConstruct
 	public void init(){
 		coapClient = new CoapClient();
-		coapClient.setURI("coap://192.168.3.109/gyroscope");
+		coapClient.setURI("coap://192.168.3.42/gyroscope");
 		coapObserveRelation = coapClient.observe(new CoapHandler() {
 			
 			@Override
